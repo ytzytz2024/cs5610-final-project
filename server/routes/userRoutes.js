5af5acd4e5d678a2eeea732593de526a61fa22fc
Tianze Yin <deyin12345@gmail.com>
2025-03-22 18:19:36 -0700

    Tianze: Add endpoint to retrieve user's saved recipes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -217,4 +217,22 @@ router.delete('/unsave-recipe/:id', auth, async (req, res) => {
   }
 });
 
+// @route   GET /api/users/saved-recipes
+// @desc    Get user's saved recipes
+// @access  Private
+router.get('/saved-recipes', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    
+    const savedRecipes = await Recipe.find({
+      _id: { $in: user.savedRecipes }
+    });
+    
+    res.json(savedRecipes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
