dfa10e9f8f31e9c52cf30ec5f979077cb0c35674
Tianze Yin <deyin12345@gmail.com>
2025-03-23 16:02:22 -0700

    Tianze: Add routing for main application pages and update NotFound component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Components
+import Home from './pages/Home';
+import Search from './pages/Search';
+import RecipeDetail from './pages/RecipeDetail';
+import AddRecipe from './pages/AddRecipe';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,8 +23,15 @@ function App() {
       <div className="app-container">
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-            <Route path="*" element={<div>Page not found</div>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/build" element={<AddRecipe />} />
+            <Route path="/recipe/edit/:id" element={<AddRecipe isEditing={true} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         
