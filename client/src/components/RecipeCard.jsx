d3cae099bf58b7faedc24c0f2d9cd9b9f51af74a
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-22 17:20:11 -0700

    Aaron: Define PropTypes of RecipeCard component

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,26 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import "./RecipeCard.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './RecipeCard.css';
 
 const RecipeCard = ({ recipe, showSaveButton = true }) => {
-  const { _id, recipeName, description, cookingTime, calories, userId } =
-    recipe;
-
+  const { _id, recipeName, description, cookingTime, calories, userId } = recipe;
+  
   // Image fallback handler
   const handleImageError = (e) => {
-    e.target.src = "https://via.placeholder.com/300x200?text=Recipe";
+    e.target.src = 'https://via.placeholder.com/300x200?text=Recipe';
   };
 
   // Check if current user is the creator of this recipe
-  const isCreator = localStorage.getItem("userId") === userId;
+  const isCreator = localStorage.getItem('userId') === userId;
 
   return (
     <div className="recipe-card-wrapper">
       <div className="card recipe-card h-100">
-        <img
-          src={recipe.image || "https://via.placeholder.com/300x200?text=Recipe"}
-          className="card-img-top"
+        <img 
+          src={recipe.image || 'https://via.placeholder.com/300x200?text=Recipe'} 
+          className="card-img-top" 
           alt={recipeName}
           onError={handleImageError}
         />
@@ -52,6 +51,19 @@ const RecipeCard = ({ recipe, showSaveButton = true }) => {
       </div>
     </div>
   );
+};
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    recipeName: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    cookingTime: PropTypes.number,
+    calories: PropTypes.number,
+    userId: PropTypes.string,
+    image: PropTypes.string
+  }).isRequired,
+  showSaveButton: PropTypes.bool
 };
 
 export default RecipeCard;
