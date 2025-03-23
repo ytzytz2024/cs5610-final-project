005c53915a81ccf8a49c0635eac1ad3eb718d439
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-22 19:41:17 -0700

    Aaron: Add RecipeDetail component to display recipe details

---
+++ client/src/pages/RecipeDetail.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './RecipeDetail.css';
+
+const RecipeDetail = ({ isLoggedIn }) => {
+
+};
+
+export default RecipeDetail;
