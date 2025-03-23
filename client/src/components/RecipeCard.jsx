8fec357b5a4c5a831344be5778f90ce64fd25cdb
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-22 17:12:27 -0700

    Aaron: Improve RecipeCard component layout and functionality

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -24,6 +24,31 @@ const RecipeCard = ({ recipe, showSaveButton = true }) => {
           alt={recipeName}
           onError={handleImageError}
         />
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{recipeName}</h5>
+          <p className="card-text flex-grow-1">{description?.substring(0, 100)}{description?.length > 100 ? '...' : ''}</p>
+          <div className="recipe-meta">
+            <span>{cookingTime ? `${cookingTime} min` : 'Time N/A'}</span>
+            <span>{calories ? `${calories} calories` : 'Calories N/A'}</span>
+          </div>
+          <div className="card-actions">
+            <Link to={`/recipe/${_id}`} className="btn btn-outline-success view-btn">
+              View
+            </Link>
+            
+            {showSaveButton && !isCreator && (
+              <button className="btn btn-light save-btn">
+                Save
+              </button>
+            )}
+            
+            {isCreator && (
+              <Link to={`/recipe/edit/${_id}`} className="btn btn-light edit-btn">
+                Edit
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
