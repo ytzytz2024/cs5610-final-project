df7fea93592d4abeca4ba8f93eb35160410f807c
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-23 19:45:23 -0700

    Aaron: Add static file serving for public directory

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 //load environment variables
 dotenv.config();
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 5001;
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Import all routes
 const recipeRoutes = require('./routes/recipeRoutes');
@@ -27,6 +30,10 @@ app.use('/api/reviews', reviewRoutes);
 //Test route
 app.get('/api', (req, res) => {
   res.json({ message: 'SmartRecipe API is running' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 //Connect to MongoDB
