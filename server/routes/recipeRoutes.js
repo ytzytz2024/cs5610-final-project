cfd6e12cfb5b87119873fbd8f969ea8b4a4aa364
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-26 20:40:42 -0700

    Aaron: finish api to get all recipes

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -39,9 +39,14 @@ const upload = multer({
 // @desc    Get all recipes
 // @access  Public
 router.get('/', async (req, res) => {
-  
+  try {
+    const recipes = await Recipe.find().sort({ createdAt: -1 });
+    res.json(recipes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-);
+});
 
 // @route   GET /api/recipes/:id
 // @desc    Get recipe by ID
