fc8c2b62e81aeb954f16f2cbae162dd3b0f33480
Xinghang Tong <tong.xing@northeastern.edu>
2025-03-26 20:41:44 -0700

    Aaron: finish api to get recipe by Id

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -52,7 +52,23 @@ router.get('/', async (req, res) => {
 // @desc    Get recipe by ID
 // @access  Public
 router.get('/:id', async (req, res) => {
-  
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    
+    if (!recipe) {
+      return res.status(404).json({ msg: 'Recipe not found' });
+    }
+    
+    res.json(recipe);
+  } catch (err) {
+    console.error(err.message);
+    
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Recipe not found' });
+    }
+    
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST /api/recipes
