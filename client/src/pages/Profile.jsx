089f48e7f105c9a25dbdb1c14f2fe2ac78ca813e
Tianze Yin <deyin12345@gmail.com>
2025-03-31 15:45:19 -0700

    Tianze: Add modal styles to Profile component CSS

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,3 @@
-// client/src/pages/Profile.jsx (with Edit Profile functionality)
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserService, RecipeService } from "../services/api";
